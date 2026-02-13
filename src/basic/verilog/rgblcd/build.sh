a3c702dae27f5870807077eebbe1252552ced51d
Nael <nael@keemail.me>
2026-02-13 13:22:52 +0100

    update

--- src/basic/verilog/rgblcd/build.sh
+++ src/basic/verilog/rgblcd/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-podman run --rm -v "$(pwd):/work" -w /work docker.io/davidsiaw/yosys-docker:latest \
+HIDRAW=/dev/$(grep -l 1D50 /sys/class/hidraw/hidraw*/device/uevent 2>/dev/null | head -1 | cut -d/ -f5)
+
+podman run --rm \
+           -v "$(pwd):/work" \
+           -w /work \
+           --device "$HIDRAW" \
+           --group-add keep-groups \
+           docker.io/davidsiaw/yosys-docker:latest \
     sh -c "yosys -p 'synth_ice40 -top TOP -json top.json' top.v lcd.v && \
            nextpnr-ice40 --up5k --package sg48 --json top.json --pcf top.pcf --asc top.asc && \
-           icepack top.asc top.bin"
-
+           icepack top.asc top.bin && \
+	   icesprog top.bin"
