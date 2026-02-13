39580474c7524953ee047077c2f77c6c9bf6d06a
Nael <nael@keemail.me>
2026-02-13 18:56:16 +0100

    wrapping container commands in the makefile

--- src/basic/verilog/rgblcd/main.mk
+++ src/basic/verilog/rgblcd/main.mk
@@ -2,50 +2,53 @@
 all: $(PROJ).rpt $(PROJ).bin
 
 %.blif: %.v $(ADD_SRC) $(ADD_DEPS)
-	yosys -ql $*.log $(if $(USE_ARACHNEPNR),-DUSE_ARACHNEPNR) -p 'synth_ice40 -blif $@' $< $(ADD_SRC)
+	$(PODMAN) yosys -ql $*.log $(if $(USE_ARACHNEPNR),-DUSE_ARACHNEPNR) -p 'synth_ice40 -blif $@' $< $(ADD_SRC)
 
 %.json: %.v $(ADD_SRC) $(ADD_DEPS)
-	yosys -ql $*.log $(if $(USE_ARACHNEPNR),-DUSE_ARACHNEPNR) -p 'synth_ice40 -json $@' $< $(ADD_SRC)
+	$(PODMAN) yosys -ql $*.log $(if $(USE_ARACHNEPNR),-DUSE_ARACHNEPNR) -p 'synth_ice40 -json $@' $< $(ADD_SRC)
 
 ifeq ($(USE_ARACHNEPNR),)
 %.asc: $(PIN_DEF) %.json
-	nextpnr-ice40 --$(DEVICE) $(if $(PACKAGE),--package $(PACKAGE)) $(if $(FREQ),--freq $(FREQ)) --json $(filter-out $<,$^) --pcf $< --asc $@
+	$(PODMAN) nextpnr-ice40 --$(DEVICE) $(if $(PACKAGE),--package $(PACKAGE)) $(if $(FREQ),--freq $(FREQ)) --json $(filter-out $<,$^) --pcf $< --asc $@
 else
 %.asc: $(PIN_DEF) %.blif
-	arachne-pnr -d $(subst up,,$(subst hx,,$(subst lp,,$(DEVICE)))) $(if $(PACKAGE),-P $(PACKAGE)) -o $@ -p $^
+	$(PODMAN) arachne-pnr -d $(subst up,,$(subst hx,,$(subst lp,,$(DEVICE)))) $(if $(PACKAGE),-P $(PACKAGE)) -o $@ -p $^
 endif
 
 
 %.bin: %.asc
-	icepack $< $@
+	$(PODMAN) icepack $< $@
 
 %.rpt: %.asc
-	icetime $(if $(FREQ),-c $(FREQ)) -d $(DEVICE) -mtr $@ $<
+	$(PODMAN) icetime $(if $(FREQ),-c $(FREQ)) -d $(DEVICE) -mtr $@ $<
 
 %_tb: %_tb.v %.v
-	iverilog -o $@ $^
+	$(PODMAN) iverilog -o $@ $^
 
 %_tb.vcd: %_tb
-	vvp -N $< +vcd=$@
+	$(PODMAN) vvp -N $< +vcd=$@
 
 %_syn.v: %.blif
-	yosys -p 'read_blif -wideports $^; write_verilog $@'
+	$(PODMAN) yosys -p 'read_blif -wideports $^; write_verilog $@'
 
 %_syntb: %_tb.v %_syn.v
-	iverilog -o $@ $^ `yosys-config --datdir/ice40/cells_sim.v`
+	$(PODMAN) iverilog -o $@ $^ `yosys-config --datdir/ice40/cells_sim.v`
 
 %_syntb.vcd: %_syntb
-	vvp -N $< +vcd=$@
+	$(PODMAN) vvp -N $< +vcd=$@
+
+test: $(TEST_SRC) $(ADD_SRC)
+	$(PODMAN) sh -c "iverilog -g2012 -o test.vvp $(TEST_SRC) $(ADD_SRC) && vvp test.vvp"
 
 prog: $(PROJ).bin
-	icesprog $<
+	$(PODMAN_DEV) icesprog $<
 
 sudo-prog: $(PROJ).bin
 	@echo 'Executing prog as root!!!'
 	sudo iceprog $<
 
 clean:
-	rm -f $(PROJ).blif $(PROJ).asc $(PROJ).rpt $(PROJ).bin $(PROJ).json $(PROJ).log $(ADD_CLEAN)
+	rm -f $(PROJ).blif $(PROJ).asc $(PROJ).rpt $(PROJ).bin $(PROJ).json $(PROJ).log $(ADD_CLEAN) test.vvp test_lcd.vcd test_lcd.vvp
 
 .SECONDARY:
-.PHONY: all prog clean
+.PHONY: all prog test clean
